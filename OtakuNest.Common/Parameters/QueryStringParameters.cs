50b8496e9654cdb9113bb390a720b9dd1a43277d
vladyslavplus <vladislavperevispa12345@gmail.com>
2025-08-24 11:16:51 +0300

    feat(common): add validation for PageNumber and PageSize in QueryStringParameters

--- OtakuNest.Common/Parameters/QueryStringParameters.cs
+++ OtakuNest.Common/Parameters/QueryStringParameters.cs
@@ -4,13 +4,28 @@
     {
         private const int MaxPageSize = 50;
         private int _pageSize = 10;
+        private int _pageNumber = 1;
 
-        public int PageNumber { get; set; } = 1;
+        public int PageNumber
+        {
+            get => _pageNumber;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("PageNumber must be greater than 0");
+                _pageNumber = value;
+            }
+        }
 
         public int PageSize
         {
             get => _pageSize;
-            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("PageSize must be greater than 0");
+                _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+            }
         }
 
         public string? OrderBy { get; set; } = "Id";
